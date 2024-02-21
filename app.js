c24d35588edd13a1cff9b97f2e9fe05d46b3de5d
Lucas Soares Nunes <143474425+LucasSoares134@users.noreply.github.com>
2024-02-20 22:34:10 -0300

    Atualizar o app.js
    comentando o script

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
-let imagemAtual = ""; // Defina a variável imagemAtual antes de utilizá-la
+let imagemAtual = ""; /* Cria a variável imagemAtual e já inicia com o valor de uma string vazia.*/
 
-function digitando() {
+function digitando() {//Função,acionada, ao começar a digitar...
     document.getElementById("__imagem-tux").src = imagemAtual;
-    imagemAtual = "imagens/processando.jfif";
+    imagemAtual = "imagens/processando.jfif";/*quando a Função for chamada,altera a imagem,para o novo src.*/
     document.getElementById("__mensagem1").innerHTML = 'Texto sendo processado...';
     document.getElementById("__mensagem2").innerHTML = 'Aguarde!';
-}
+}//altera os textos, ao acionar a Função.
 
-function criptografar() {
-    document.getElementById("__imagem-tux").src = imagemAtual;
+function criptografar() {//acionada, quando clica o button...
+    document.getElementById("__imagem-tux").src = imagemAtual;/*Busca esse elemento pelo id,lá no html e altera seu src, pelo da variavel, que o valor está abaixo*/
     imagemAtual = "imagens/among us.jpg";
-    let mensagemDigitada = document.getElementById('__digitar-mensagem').value;
-    digitando();
-    //-------------------------------------------------------------------------
-    let codificador = { 'a': 'ai', 'e': 'enter', 'i': 'imes', 'o': 'ober', 'u': 'ufat' };
+    let mensagemDigitada = document.getElementById('__digitar-mensagem').value;/*cria uma nova variável e atribui o valor que vai ser passado de string, nesse campo do html*/
+    digitando();//chama a função...
+
+    let codificador = { 'a': 'ai', 'e': 'enter', 'i': 'imes', 'o': 'ober', 'u': 'ufat' };//criando um array com as mensagens,onde cada uma já tem seu valor trocado
     mensagemDigitada = mensagemDigitada.replace(/[aeiou]/g, function(criptografia) {
      return codificador[criptografia] || criptografia;
-    });
-//---------------------------------------------------------------------------
-
-    document.getElementById("__mensagem1").innerHTML = mensagemDigitada;
-    document.getElementById("__mensagem2").innerHTML = '';
-    //-------------------------------------------------------------------------
+//percorre os valores na string e troca, confome o array
+    document.getElementById("__mensagem1").innerHTML = mensagemDigitada;/*passa o valor armazenado na variável.*/
+    document.getElementById("__mensagem2").innerHTML = '';/*altera o valor da string para vazia*/
 }
 function descriptografar() {
     document.getElementById("__imagem-tux").src = imagemAtual;
     imagemAtual = "imagens/among us2.jpg";
     let mensagemDigitada = document.getElementById('__digitar-mensagem').value;
-    digitando();
-     //-------------------------------------------------------------------------
-
      let decodificador = { 'ai': 'a', 'enter': 'e', 'imes': 'i', 'ober': 'o', 'ufat': 'u' };
      mensagemDigitada = mensagemDigitada.replace(/ai|enter|imes|ober|ufat/g, function(criptografia) {
          return decodificador[criptografia] || criptografia;
-
-        });
-     //---------------------------------------------------------------------------
     document.getElementById("__mensagem1").innerHTML = mensagemDigitada;
     document.getElementById("__mensagem2").innerHTML = '';
-    //-------------------------------------------------------------------------
-}
+}//mesma lógica da function criptografia 
 
 function copiarMensagem() {
     var mensagemCopiar = document.getElementById("__mensagem1").innerText;
-
+//cria uma variável, que armazena o valor da primeira mensagem.
     // Utilizando navigator.clipboard.writeText() para copiar o texto
-    navigator.clipboard.writeText(mensagemCopiar)
+    navigator.clipboard.writeText(mensagemCopiar);
        
- };
+ }
 
